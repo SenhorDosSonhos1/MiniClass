dc94bdf35b7bd3815b9220ee9f3a10876a08812e
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2026-06-18 19:14:56 -0300

    feat: implement user authentication and role assigment

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import user_login
+from accounts.views import user_login, user_register
 
 
 urlpatterns = [
-    path('login', user_login, name='user_login')
+    path('login/', user_login, name='user_login'),
+    path('register/', user_register, name= 'user_register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# Create your views here.
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from accounts.models import Profile
+
 def user_login(request):
-    return HttpResponse("O usuario nao esta logado")
+    if request.method == "GET":
+        return render(request, 'accounts/login.html')
+    
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+
+    user = authenticate(username = username, password = password)
+
+    if user is None:
+        return HttpResponse("Usuario inválido")
+    
+    login(request, user)
+    return redirect("my_courses")
+    
+def user_register(request):
+    if request.method == "GET":
+        return render(request, 'accounts/register.html')
+    
+    username = request.POST.get("username")
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    confirm_pass = request.POST.get("confirm_pass")
+    role = request.POST.get("profile")
+
+    if password != confirm_pass:
+        return HttpResponse("Senhas não coincidem")
+    
+    if not User.objects.filter(username = username).exists():
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        profile = Profile.objects.get(user = user)
+        profile.role = role
+        profile.save()
+
+        return redirect("user_login")
+
+    return HttpResponse("Usuario já existente")
+
+def user_logout(request):
+    logout(request)
+    return redirect("user_login")
